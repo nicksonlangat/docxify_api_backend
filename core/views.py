e4245459912d7999f82bd113abf55d6e426a5a00
NickLangat <nickson@reveall.co>
2023-04-04 09:52:53 +0300

    added doc status

--- core/views.py
+++ core/views.py
@@ -4,5 +4,14 @@ from .serializers import DocumentSerializer
 
 # Create your views here.
 class DocumentViewset(viewsets.ModelViewSet):
-    queryset = Document.objects.all()
     serializer_class = DocumentSerializer
+
+    def get_queryset(self):
+        queryset = Document.objects.all()
+        type = self.request.query_params.get('type', None)
+        status = self.request.query_params.get('status', None)
+        if type is not None :
+            queryset = queryset.filter(type=type.capitalize())
+        if status is not None :
+            queryset = queryset.filter(status=status.capitalize())
+        return queryset
