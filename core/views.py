43a0e8763af8745850e5469c6c4f89e69d337a37
NickLangat <nickson@reveall.co>
2023-04-10 21:40:14 +0300

    added auth

--- core/views.py
+++ core/views.py
@@ -1,14 +1,16 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Document
 from .serializers import DocumentSerializer
 
 # Create your views here.
 class DocumentViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
 
     def get_queryset(self):
-        queryset = Document.objects.all()
+        queryset = Document.objects.filter(author=self.request.user)
         type = self.request.query_params.get('type', None)
         status = self.request.query_params.get('status', None)
         if type is not None :
